7d646ebd25deb7bbc945989ef34b8df88a4176ce
Tomash Petersen <d.g.volkovbz>
2022-10-27 00:23:23 +0400

    Add smooth link scroll script

--- js/main.js
+++ js/main.js
@@ -9,3 +9,11 @@ $(function(){
     });
 
 });
+
+$(".menu, .bike__column").on("click","a", function (event) {
+    event.preventDefault();
+    var id  = $(this).attr('href'),
+        top = $(id).offset().top;
+    $('body,html').animate({scrollTop: top}, 1500);
+});
+    
